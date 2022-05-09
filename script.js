87397ccacd3787df13f4d36065477618339d820a
PineappleWoe <JordanTateAdds@Gmail.com>
2022-05-09 12:54:48 +0100

    Add user input for grid size

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const gridContainer = document.querySelector('.grid-container');
-const gridSize = 16;
+let gridSize = 16;
     gridContainer.style.display = "grid";
     gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`; 
     gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
 function createGrid(size) {
-    for (let i = 0; i < gridSize; i++){
+    for (let i = 0; i < size; i++){
         const gridColumn = document.createElement('div');
         gridColumn.classList.add('grid-square');
         // gridColumn.textContent = (i + 1);
         gridContainer.appendChild(gridColumn);
-        for (let j = 1; j < gridSize; j++){
+        for (let j = 1; j < size; j++){
             const gridRow = document.createElement('div');
             gridRow.classList.add('grid-square');
             // gridRow.textContent = (j + 1);
@@ -20,3 +20,15 @@ function createGrid(size) {
 }
 
 createGrid(gridSize);
+
+const gridSizeSliderValue = document.querySelector('#grid-size');
+gridSizeSliderValue.addEventListener('change', function() {
+    gridContainer.innerHTML = "";
+
+    gridSize = gridSizeSliderValue.value;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`; 
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+    console.log(gridSize);
+    createGrid(gridSize);
+});
